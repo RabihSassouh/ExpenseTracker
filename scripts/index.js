e28106acfe3dfeb80db9127ae37217a802556661
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-09 21:16:35 +0200

    added editTransaction function to edit transactions already added to viewTransaction container

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,6 @@
 let transactions = [];
 const transactionsContainer = document.getElementById("transactions-container");
-const createTransactionForm = document.getElementById('add-transaction-form');
-
-function createTransaction(description, amount) {
-  const newTransaction = { description, amount };
-  transactions.push(newTransaction);
-  // console.log("test");
-  viewTransactions();
-}
+const createTransactionForm = document.getElementById("add-transaction-form");
 
 function viewTransactions() {
   transactionsContainer.innerHTML = "";
@@ -24,15 +17,30 @@ function viewTransactions() {
   });
 }
 
-function deleteTransaction(index){
-    transactions.splice(index,1);
-    viewTransactions();
+function createTransaction(description, amount) {
+  const newTransaction = { description, amount };
+  transactions.push(newTransaction);
+  // console.log("test");
+  viewTransactions();
 }
 
-createTransactionForm.addEventListener('submit', function(event) {
+function deleteTransaction(index) {
+  transactions.splice(index, 1);
+  viewTransactions();
+}
+
+function editTransaction(index) {
+  const newDescription = prompt("Enter new description:");
+  const newAmount = parseFloat(prompt("enter new amount:"));
+  transactions[index].description = newDescription;
+  transactions[index].amount = newAmount;
+  viewTransactions();
+}
+
+createTransactionForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  const description = document.getElementById('description').value;
-  const amount = parseFloat(document.getElementById('amount').value);
+  const description = document.getElementById("description").value;
+  const amount = parseFloat(document.getElementById("amount").value);
   createTransaction(description, amount);
   createTransactionForm.reset();
 });
